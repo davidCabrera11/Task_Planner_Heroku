a484f1db2777d60ecee5ffef6508c52c2774d475
David Cabrera <davidcabrera052196@gmail.com>
2021-04-25 18:14:24 -0500

    First commit

---
+++ src/main/kotlin/org/adaschool/TaskPlanner/TaskPlannerApplication.kt
@@ -0,0 +1,11 @@
+package org.adaschool.TaskPlanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskPlannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskPlannerApplication>(*args)
+}
---
+++ src/test/kotlin/org/adaschool/TaskPlanner/TaskPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.adaschool.TaskPlanner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskPlannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
