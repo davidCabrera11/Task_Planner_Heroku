d6bb4709783cf85478d417bfacb49a461c31f4d0
David Cabrera <davidcabrera052196@gmail.com>
2021-05-01 14:42:01 -0500

    Created dataclass login Dto

--- src/main/kotlin/org/adaschool/TaskPlanner/controller/auth/AuthController.kt
+++ src/main/kotlin/org/adaschool/TaskPlanner/controller/auth/AuthController.kt
@@ -1,11 +1,27 @@
 package org.adaschool.TaskPlanner.controller.auth
 
+import org.adaschool.TaskPlanner.controller.dto.LoginDto
+import org.adaschool.TaskPlanner.services.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/auth")
-class AuthController {
+class AuthController(
+    @Value("\${SECRET}") val secret: String,
+    @Autowired val userService: UserService
+) {
+
+    @PostMapping
+    fun authenticate(@RequestBody loginDto: LoginDto){
+
+
+
+    }
 
 
 
---
+++ src/main/kotlin/org/adaschool/TaskPlanner/controller/dto/LoginDto.kt
@@ -0,0 +1,3 @@
+package org.adaschool.TaskPlanner.controller.dto
+
+data class LoginDto(val email:String, val password:String)
